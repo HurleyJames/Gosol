417c0b14cf8ba0c8ffb35cc26f0e4cb47b33013b
Hurley <1401682479@qq.com>
2020-05-14 09:32:38 +0100

    add pet-shop and smart degree dapp

--- pet-shop/test/TestAdoption.sol
+++ pet-shop/test/TestAdoption.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.5.1 <0.7.0;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
-import "../contracts/Adoption";
+import "../contracts/Adoption.sol";
 
 contract TestAdoption {
     Adoption adoption = Adoption(DeployedAddresses.Adoption());
@@ -15,14 +15,14 @@ contract TestAdoption {
     }
 
     function testGetAdopterAddressByPetId() public {
-        address expected = this;
+        address expected = address(this);
         address adopter = adoption.adopters(8);
         Assert.equal(adopter, expected, "Adoption of pet ID 8 should be recorded.");
     }
 
     // 验证数组中的8号宠物与地址是否符合
     function testGetAdopterAddressByPetIdInArray() public {
-        address expected = this;
+        address expected = address(this);
         address[16] memory adopters = adoption.getAdopters();
         Assert.equal(adopters[8], expected, "Adoption of pet ID 8 should be recorded.");
     }
---
+++ smart-degree-develop/contracts/SmartDegree.sol
@@ -0,0 +1,66 @@
+pragma solidity >=0.5.1 <0.7.0;
+
+import "../node_modules/open-smartkit/contracts/integrity/Sealable.sol";
+import "../node_modules/open-smartkit/contracts/lifecycle/Lockable.sol";
+
+/**
+ * @title SmartDegree
+ * @dev SmartDegree ....
+ */
+contract SmartDegree is Sealable, Lockable {
+
+    /**
+     * @notice Create a new SmartDegree Contract.
+     */
+    constructor() Lockable(0) public {
+        addAddressToWhitelist(msg.sender);
+    }
+
+    /**
+     * @notice Register a new delegate authorized to add degree
+     */
+    function grantAuthority(address grantee) onlyOwner onlyUnlock public returns (bool success) {
+        return super.registerDelegate(grantee);
+    }
+
+    /**
+    * Use this getter function to access the degree hash value
+    * @param id of the seal
+    * @return the seal
+    */
+    function getDegreeHash(bytes32 id) public view returns (bytes32) {
+        return super.getSeal(id);
+    }
+
+    /**
+     * @notice Add a new DegreeHash to the contract.
+     */
+    function deliverDegree(bytes32 id, bytes32 degreeHash) public onlyWhitelisted onlyUnlock {
+        return super.recordSeal(id, degreeHash);
+    }
+
+    /**
+     * Use these method functions to verify a degree hash
+     * @param id of the degree
+     * @param degreeHash of the degree to verify
+     */
+    function isValid(bytes32 id, bytes32 degreeHash) public view returns (bool) {
+        return super.verifySeal(id, degreeHash);
+    }
+
+    /**
+     * @notice Define a date before blocking the contract
+     * @param newDateLimit uint256 new date limit of the contract (timestamp as seconds since unix epoch)
+     */
+    function setDateLimit(uint256 newDateLimit) public onlyOwner {
+        return super.setDateLimit(newDateLimit);
+    }
+
+    /**
+   * @dev return contract status.
+   * @return status (true if the contract is locked)
+   */
+    function isLocked() public view returns (bool){
+        return super.isLocked();
+    }
+}
