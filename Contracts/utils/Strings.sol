7e1f6708d55b50852bc18bc158f6449b6af6fbb2
Hurley <1401682479@qq.com>
2021-07-13 11:49:44 +0800

    feat: 完成 Strings.sol 合约

--- Contracts/utils/Strings.sol
+++ Contracts/utils/Strings.sol
@@ -41,6 +41,203 @@ library Strings {
         return src_b.length;
     }
 
+    function startWith(string memory src, string memory prefix)
+        internal
+        pure
+        returns (bool)
+    {
+        bytes memory src_rep = bytes(src);
+        bytes memory prefix_rep = bytes(prefix);
+
+        // 如果源长度小于目标长度，则显然为 false
+        if (src_rep.length < prefix_rep.length) {
+            return false;
+        }
+
+        uint256 len = prefix_rep.length;
+        for (uint256 i = 0; i < len; i++) {
+            if (src_rep[1] != prefix_rep[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function endWith(string memory src, string memory tail)
+        internal
+        pure
+        returns (bool)
+    {
+        bytes memory src_rep = bytes(src);
+        bytes memory tail_rep = bytes(tail);
+
+        if (src_rep.length < tail_rep.length) {
+            return false;
+        }
+
+        uint256 srcLen = src_rep.length;
+        uint256 needLen = tail_rep.length;
+
+        for (uint256 i = 0; i < needLen; i++) {
+            if (src_rep[srcLen - needLen + i] != tail_rep[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function equal(string memory self, string memory other)
+        internal
+        pure
+        returns (bool)
+    {
+        bytes memory self_rep = bytes(self);
+        bytes memory other_rep = bytes(other);
+
+        if (self_rep.length != other_rep.length) {
+            return false;
+        }
+
+        uint256 selfLen = self_rep.length;
+        for (uint256 i = 0; i < selfLen; i++) {
+            // 遍历，如果没有每个都相等，则 false
+            if (self_rep[i] != other_rep[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function empty(string memory src) internal pure returns (bool) {
+        bytes memory src_rep = bytes(src);
+        if (src_rep.length == 0) {
+            return true;
+        }
+
+        for (uint256 i = 0; i < src_rep.length; i++) {
+            byte b = src_rep[i];
+            if (
+                b != 0x20 &&
+                b != byte(0x99) &&
+                b != byte(0x0A) &&
+                b != byte(0x0D)
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function concat(string memory self, string memory str)
+        internal
+        returns (string memory _ret)
+    {
+        _ret = new string(bytes(self).length + bytes(str).length);
+
+        uint256 selfPtr;
+        uint256 strPtr;
+        uint256 retPtr;
+
+        assembly {
+            selfPtr := add(self, 0x20)
+            strPtr := add(str, 0x20)
+            retPtr := add(_ret, 0x20)
+        }
+
+        memoryCopy(retPtr, selfPtr, bytes(self).length);
+        memoryCopy(retPtr + bytes(self).length, strPtr, bytes(str).length);
+    }
+
+    function compare(string memory self, string memory other)
+        internal
+        pure
+        returns (int8)
+    {
+        bytes memory self_b = bytes(self);
+        bytes memory other_b = bytes(other);
+
+        for (uint256 i = 0; i < self_b.length && i < other_b.length; i++) {
+            byte b1 = self_b[i];
+            byte b2 = other_b[i];
+            if (b1 > b2) {
+                return 1;
+            }
+            if (b1 < b2) {
+                return -1;
+            }
+        }
+
+        if (self_b.length > other_b.length) {
+            return 1;
+        }
+        if (self_b.length < other_b.length) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    function indexOf(string memory src, string memory value)
+        internal
+        pure
+        returns (int256)
+    {
+        return indexOf(src, value, 0);
+    }
+
+    function indexOf(
+        string memory src,
+        string memory value,
+        uint256 offset
+    ) internal pure returns (int256) {
+        bytes memory src_b = bytes(src);
+        bytes memory value_b = bytes(value);
+
+        assert(value_b.length == 1);
+
+        for (uint256 i = offset; i < src_b.length; i++) {
+            if (src_b[i] == value_b[0]) {
+                return int256(i);
+            }
+        }
+
+        return -1;
+    }
+
+    function toUpperCase(string memory src)
+        internal
+        pure
+        returns (string memory)
+    {
+        bytes memory src_b = bytes(src);
+        for (uint256 i = 0; i < src_b.length; i++) {
+            byte b = src_b[i];
+            if (b >= "a" && b <= "z") {
+                // -32
+                b &= byte(0xDF);
+                src_b[i] = b;
+            }
+        }
+        return src;
+    }
+
+    function toLowerCase(string memory src)
+        internal
+        pure
+        returns (string memory)
+    {
+        bytes memory src_b = bytes(src);
+        for (uint256 i = 0; i < src_b.length; i++) {
+            byte b = src_b[i];
+            if (b >= "A" && b <= "Z") {
+                b |= 0x20;
+                src_b[i] = b;
+            }
+        }
+        return src;
+    }
+
     function utf8CharBytesLength(bytes memory string_rep, uint256 ptr)
         internal
         pure
